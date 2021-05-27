e0bfae057ee651a0f109aa0c1b4269c3a27933af
Vitoria Andrade <vitoria0611@hotmail.com>
2021-05-27 08:54:25 -0300

    listas add

---
+++ components/ListaPlaylists/ListaPlaylist.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ListaContainer = styled.div`
+  display: flex;
+  whidth: 100%
+  justify-content: space-between;
+`;
+
+export const ListaItem= styled.div`
+  width: 100%;
+  background: #fff;
+  justify-content: space-between;
+  
+  
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from "next/head";
 import styled from "styled-components";
 
 import { Filtro } from "../components/Filtro/Filtro";
+import{ListaPlaylists} from "../components/ListaPlaylists/ListaPlaylists";
 
 const Container = styled.div`
-  display: flex;
+  display: grid;
   background: #000;
+  
 `;
 
 export default function Home() {
@@ -16,7 +18,7 @@ export default function Home() {
       </Head>
 
       <Filtro />
-      {/* <ListaPlaylists /> */}
+       <ListaPlaylists /> 
     </Container>
   );
 }
